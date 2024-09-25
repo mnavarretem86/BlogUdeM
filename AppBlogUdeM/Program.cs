456181ade0a3211cfccf4ad157cb3713a71923c8
Milton Navarrete <milton_navarrete@hotmail.com>
2024-09-24 21:11:25 -0600

    Commit Inicial

---
+++ AppBlogUdeM/Program.cs
@@ -0,0 +1,39 @@
+using AppBlogUdeM.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Usuarios}/{controller=Home}/{action=Index}/{id?}"); //Se agrega el area del cliente , ahi esta el home controller 
+app.MapRazorPages();                                                    //el punto de entra de la aplicacion
+
+app.Run();
