734f39ece80122f9cbaa989ec8834157ceebd451
Milton Navarrete <milton_navarrete@hotmail.com>
2024-09-25 23:10:38 -0600

    Se agrega Irepositori

---
+++ AppBlogUdeM.AccesoDatos/Data/Repositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using AppBlogUdeM.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBlogUdeM.AccesoDatos.Data.Repositorio.IRepositorio
+{
+    // Interfaz ICategoria que extiende la funcionalidad de IRepositorio para la entidad Categoria.
+    // Esta interfaz define operaciones específicas para manejar categorías en el sistema.
+    public interface ICategoriaRepositorio : IRepositorio<Categoria>
+    {
+        // Método para actualizar una categoría existente.
+        // Parámetros:
+        // - categoria: La instancia de la entidad Categoria que se desea actualizar.
+        void Update(Categoria categoria);
+
+    }
+
+}
---
+++ AppBlogUdeM.AccesoDatos/Data/Repositorio/IRepositorio/IContenedorTrabajo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBlogUdeM.AccesoDatos.Data.Repositorio.IRepositorio
+{
+    // Interfaz IContenedorTrabajo que representa una unidad de trabajo para manejar múltiples repositorios.
+    // Esta interfaz implementa IDisposable para liberar recursos de manera controlada.
+    internal interface IContenedorTrabajo : IDisposable
+    {
+        // Propiedad para acceder al repositorio de categorías.
+        // Devuelve una instancia de ICategoriaRepositorio, permitiendo realizar operaciones sobre categorías.
+        ICategoriaRepositorio Categoria { get; }
+
+        // Método para guardar los cambios realizados en el contexto de la unidad de trabajo.
+        // Este método persiste las modificaciones de todos los repositorios asociados en una única transacción.
+        void Save();
+    }
+
+}
